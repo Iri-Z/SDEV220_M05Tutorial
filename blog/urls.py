38bf03f9f71e4badb06efdd10d6d083c88c9f0d3
Iri <azink11@ivytech.edu>
2023-07-23 10:30:20 -0400

    Added view and template for individual blog posts - post_detail

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #All posts on one page
     path('', views.post_list, name='post_list'),
+    #Individual posts displayed on their own page
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
